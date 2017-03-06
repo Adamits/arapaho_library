a1c6c1302f0a720c28b9aa3abc104b2d0cc1e0ed
Adam <ajwieme@gmail.com>
2017-03-06 12:08:46 -0700

    fixing text parser to capture corrext data

--- ArapahoTextParser.py
+++ ArapahoTextParser.py
@@ -1,48 +1,139 @@
+# -*- coding: utf-8 -*-
 import json
 from time import strftime
-from objectpath import *
 import re
+import codecs
 
 # Note that x= dict[key], where key points to another dict
 # seems to store a pointer to that dict in python, so updating
 # an attribute of a LexicalEntry SHOULD also update the json...
 
 # Path to arapaho_lexicon.json
-arapaho_json_path = "./data/arapaho_lexicon.json"
+def get_lexicon_directory():
+  for line in open('config.txt'):
+    if line.startswith('text_path'):
+      return line.split('=')[1].strip()
+  exit('WARNING: could not find a value for text_path')
 
-class ArapahoLexicalParser(object):
+class ArapahoTextParser(object):
   def __init__(self):
-    self.lexical_entries = []
-    self.json = {}
+    self.examples = []
 
   def parse(self):
-    with open(arapaho_json_path) as data_file:
-      arapaho_json = json.load(data_file)
-      self.json = arapaho_json
+    # Get all lines from text, removing trailing whitespace
+    lines = [line.strip() for line in codecs.open(get_lexicon_directory(), encoding='latin-1')]
 
-    for lex_id, lex_entry in arapaho_json.items():
-      # Instantiate a lexical entry of all the json at this lex_id
-      self.lexical_entries.append(LexicalEntry({lex_id: lex_entry}))
+    current_ref = ""
+    for line_index, line in enumerate(lines):
+      # If the line does not start with \, then it is trailing text from the last line
+      # so add it to that line in the dict, space padded
+      if not line.startswith("\\") and line != '' and line_index > 0:
+        last_line = lines[line_index - 1]
+        if re.match(r'\\tx', last_line):
+          current_ref_dict["tx"][-1] += " %s" % line
+        elif re.match(r'\\mb', last_line):
+          current_ref_dict["mb"][-1] += " %s" % line
+        elif re.match(r'\\ge', last_line):
+          current_ref_dict["ge"][-1] += " %s" % line
+        elif re.match(r'\\ps', last_line):
+          current_ref_dict["ps"][-1] += " %s" % line
+        elif re.match(r'\\ft', last_line):
+          current_ref_dict["ft"][-1] += " %s" % line
 
-  def where(self, query_string):
-    # Need to copy the json in order to modify so that we can do a little
-    # hack in order to solve the issue that object path does not return the key,
-    # which is the lex_id for us.
-    query_json = self.json.copy()
-    # Add lex_id as a key in the dict so that it can be added back
-    # as the highest level key so that instantiation of LexicalEntry objects will work.
-    for lex_id in query_json.keys():
-      query_json[lex_id].update({"lex_id": lex_id})
-    # objectpath tree of json formatted for the query
-    tree = Tree(query_json)
-    # Use objectpath to get a generator of entries
-    # where that matches objectpath query in query_string
-    q = tree.execute("$..*[@.%s]" % query_string)
-    # Convert generator to a list, loop over it, and instantiate LexicalEntry
-    # objects with each one that returned
-    lex_entries = []
-    for lex_entry in list(q):
-      # Add the lex_id as the key again, and remove it from the dict within
-      lex_entries.append(LexicalEntry({lex_entry.pop("lex_id"): lex_entry}))
+      if re.match(r'\\ref', line):
+        # When we hit a new ref, we need to store all of the examples that are within that ref
+        if current_ref != "":
+          # Loop over all lines under this ref, assume that if there is no tx,
+          # then we do not need to instantiate it as an example.
+          # Basically, len(current_ref_dict["tx"]) should = the number of examples from this ref
+          for i in range(len(current_ref_dict["tx"])):
+            text_example = TextExample()
+            text_example.ref = current_ref
+            if len(current_ref_dict["tx"]) > i:
+              text_example.tx = current_ref_dict["tx"][i]
+            if len(current_ref_dict["mb"]) > i:
+              text_example.mb = current_ref_dict["mb"][i]
+            if len(current_ref_dict["ge"]) > i:
+              text_example.ge = current_ref_dict["ge"][i]
+            if len(current_ref_dict["ps"]) > i:
+              text_example.ps = current_ref_dict["ps"][i]
+            if len(current_ref_dict["ft"]) > i:
+              text_example.ft = current_ref_dict["ft"][i]
+            # If we do not have an ft for this index, then use the last one of the ft slot.
+            # It seems that the glosses in a ref should all be pointing to the same ft, but
+            # we will check index and if not do last ft just to be safe.
+            elif len(current_ref_dict["ft"]) > 0:
+              text_example.ft = current_ref_dict["ft"][-1]
+            # Double check if there was actually example data in the example_block, or just noise
+            if text_example.contains_data():
+              self.examples.append(text_example)
 
-    return lex_entries
+        # Store the value of the ref, and remove trailing whitespace
+        current_ref = re.split(r'\\ref', line)[1].strip()
+        current_ref_dict = {}
+        current_ref_dict["tx"] = []
+        current_ref_dict["mb"] = []
+        current_ref_dict["ge"] = []
+        current_ref_dict["ps"] = []
+        current_ref_dict["ft"] = []
+      elif re.match(r'\\tx', line):
+        current_ref_dict["tx"].append(re.split(r'\\tx', line)[1].strip())
+      elif re.match(r'\\mb', line):
+        current_ref_dict["mb"].append(re.split(r'\\mb', line)[1].strip())
+      elif re.match(r'\\ge', line):
+        current_ref_dict["ge"].append(re.split(r'\\ge', line)[1].strip())
+      elif re.match(r'\\ps', line):
+        current_ref_dict["ps"].append(re.split(r'\\ps', line)[1].strip())
+      elif re.match(r'\\ft', line):
+        current_ref_dict["ft"].append(re.split(r'\\ft', line)[1].strip())
+
+  def to_json(self):
+    return json.dumps([text_example.dict() for text_example in self.examples])
+
+class TextExample(object):
+
+  def __init__(self, options={}):
+    self.ref = options.get("ref", "")
+    self.tx = options.get("tx", "")
+    self.mb = options.get("mb", "")
+    self.ge = options.get("ge", "")
+    self.ps = options.get("ps", "")
+    self.ft = options.get("ft", "")
+
+  def dict(self):
+    return {"ref": self.ref, "tx": self.tx, "mb": self.mb, "ge": self.ge, "ps": self.ps, "ft": self.ft}
+
+  def contains_data(self):
+    return self.tx != "" or self.mb != "" or self.ge != "" or self.ps != "" or self.ft != ""
+
+  def contains_ref(self):
+    return self.ref != ""
+
+  # Cleaned list of each morpheme of the mb string
+  # This can be used to point a morpheme, which should correspond
+  # with a lex, to all of the information for that example
+  def get_mb_list(self):
+    mb_string = self.mb
+
+    # Remove the space delimited '-' and put it in the list index
+    # With the preceding string
+    morphemes = re.split(r'\s+', mb_string)
+    if '-' in morphemes:
+      k = morphemes.index('-')
+      morphemes[k - 1] = morphemes[k - 1].replace(morphemes[k - 1], morphemes[k - 1] + '-')
+      morphemes.remove('-')
+
+      mb_string = '  '.join(morphemes)
+
+    # mb has no dashes and no '' and no ' '
+    mb_list = re.split(r'\s+', mb_string)
+    if '' in mb_list:
+      mb_list.remove('')
+    if ' ' in mb_list:
+      mb_list.remove(' ')
+
+    return mb_list
+
+def run_test():
+  x = ArapahoTextParser()
+  x.parse()
