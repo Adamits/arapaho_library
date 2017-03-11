ac2f8ead5ade7486a4cb20b7a6d6a0cc34af87e0
Adam <ajwieme@gmail.com>
2017-03-10 18:52:45 -0700

    clean up a little

--- add_texts_to_db.py
+++ add_texts_to_db.py
@@ -13,24 +13,32 @@ print "%d examples in lexicon" % sum([len(entry.examples) for entry in lexical_p
 mbs_and_examples = text_parser.mbs_and_examples()
 mbs = mbs_and_examples.keys()
 
-print len(mbs)
+print "%d unique mbs in the text" % len(mbs)
 total_examples_count = 0
 examples_to_add_count = 0
 
 for entry in lexical_parser.lexical_entries:
-  # If this entry appears in the example
+  # If this entry appears in the example morphemes
   match = entry.appears_in(mbs)
   if match:
+    # get the examples that have a morpheme (mb) of this entry
     match_examples = mbs_and_examples[match]
-    entry.examplefrequency += len(match_examples)
+    # Increment total examples for logging
     total_examples_count += len(match_examples)
+    # Loop over the examples that have this morpheme
     for match_example in match_examples:
+      # Generate an Example Object that goes into th elexicon (probably need to namespace this)
       lex_example = Example(match_example.list())
+      # Check if that example is in the lexical entry object already
       if not entry.contains_example(lex_example):
+        # Add example to the liexical entry object
         entry.examples.append(lex_example)
+        # Increment examplefrequency
+        entry.examplefrequency += 1
+        # For logging
         examples_to_add_count += 1
 
 print "%s total examples" % total_examples_count
-print "%s total examples" % examples_to_add_count
+print "%s examples added" % examples_to_add_count
 with open("/Users/ajwieme/Desktop/test_arapaho_lexicon", 'w') as outfile:
   json.dump(lexical_parser.as_json_dict(), outfile)
--- tests.py
+++ tests.py
@@ -5,7 +5,7 @@ from arapahotextparser import ArapahoTextParser
 text_parser = ArapahoTextParser()
 lexical_parser = ArapahoLexicalParser()
 
-text_parser.parse()
+#text_parser.parse()
 lexical_parser.parse()
 
 print "%d examples in lexicon" % sum([len(entry.examples) for entry in lexical_parser.where("examplefrequency > 0")])
@@ -19,30 +19,4 @@ def as_json_dict_test():
   print entry.examples[0].ft
   return entry.as_json_dict()
 
-def add_to_lex_test():
-  mbs_and_examples = text_parser.mbs_and_examples()
-  mbs = mbs_and_examples.keys()
-
-  print len(mbs)
-  total_examples_count = 0
-  examples_to_add_count = 0
-
-  for entry in lexical_parser.lexical_entries:
-    # If this entry appears in the example
-    match = entry.appears_in(mbs)
-    if match:
-      match_examples = mbs_and_examples[match]
-      entry.examplefrequency += len(match_examples)
-      total_examples_count += len(match_examples)
-      for match_example in match_examples:
-        lex_example = Example(match_example.list())
-        if not entry.contains_example(lex_example):
-          entry.examples.append(lex_example)
-          examples_to_add_count += 1
-
-  print "%s total examples" % total_examples_count
-  print "%s total examples" % examples_to_add_count
-  with open("/Users/ajwieme/Desktop/test_arapaho_lexicon", 'w') as outfile:
-    json.dump(lexical_parser.as_json_dict(), outfile)
-
-add_to_lex_test()
+print len(lexical_parser.where("examplefrequency > 100"))
